9bf23f8dbd8ffdfec13c72e35504c0f9cb7457cf
Roger Davila <droger88@hotmail.com>
2019-09-01 03:59:50 -0600

    004 Creación de la Base de Datos

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Shop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class DataContext: DbContext
+    {
+        //DbSet me tira la propiedad de Prodcut a la base de datos
+        //Mis datos no los trataré como si fueran tablas, sino como colección de objetos
+        public DbSet<Product> Products { get; set; }
+
+        //Creamos la conexión a la Base de datos
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
